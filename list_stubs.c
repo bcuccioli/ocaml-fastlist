3d1e06b45117b7fcd60e6bcd8ad4589471437f59
Bryan Cuccioli <blc72@cornell.edu>
2014-05-19 23:38:44 -0400

    Clean up early functions.

--- list_stubs.c
+++ list_stubs.c
@@ -10,29 +10,22 @@
 CAMLprim value
 clist_length(value v) {
   CAMLparam1(v);
-  CAMLlocal1(head);
-
   int len = 0;
-
   while (v != Val_emptylist) {
-    head = Field(v, 0); /* Access the head. */
     v = Field(v, 1); /* Points to the tail. */
     len++;
   }
-
   return Val_int(len);
 }
 
 CAMLprim value
 clist_hd(value v) {
   CAMLparam1(v);
-  CAMLlocal1(head);
   if (v == Val_emptylist) {
     /* List is empty, so failwith "hd". */
     caml_failwith("hd");
   }
-  head = Field(v, 0);
-  CAMLreturn(head);
+  CAMLreturn(Field(v, 0));
 }
 
 CAMLprim value
@@ -42,8 +35,7 @@ clist_tl(value v) {
     /* List is empty, so failwith "hd". */
     caml_failwith("tl");
   }
-  v = Field(v, 1);
-  CAMLreturn(v);
+  CAMLreturn(Field(v, 1));
 }
 
 CAMLprim value
@@ -62,7 +54,7 @@ clist_nth(value v, value n) {
     caml_failwith("nth");
   }
   v = Field(v, 0);
-  CAMLreturn(v);
+  CAMLreturn(Field(v, 0));
 }
 
 CAMLprim value
